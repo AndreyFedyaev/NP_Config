13459997a3e09da25173fcf7ac6aa5050931f97f
Fedyaev Andrey <Fedyaev-A-A@mail.ru>
2023-01-20 10:23:34 +0500

    разработка интерфейса

--- NP_Config/WorkPage.xaml.cs
+++ NP_Config/WorkPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -24,27 +25,207 @@ namespace NP_Config
         public WorkPage()
         {
             InitializeComponent();
+            Initialize_UCH_ZR_set();   //Создаем динамические текстовые боксы для настройки датчиков на участке
+        }
+        struct UCH_ZR_setting_left
+        {
+            public Grid G;
+            public TextBox UCH_ZR_Set_Address;
+            public TextBox UCH_ZR_Set_NP;
+            public TextBox UCH_ZR_Set_Channel;
+        }
+        struct UCH_ZR_setting_right
+        {
+            public Grid G;
+            public TextBox UCH_ZR_Set_Address;
+            public TextBox UCH_ZR_Set_NP;
+            public TextBox UCH_ZR_Set_Channel;
         }
 
+        UCH_ZR_setting_left[] UCH_ZR_set_left = new UCH_ZR_setting_left[7];
+        UCH_ZR_setting_right[] UCH_ZR_set_right = new UCH_ZR_setting_right[7];
+
+        private void Initialize_UCH_ZR_set()    //Создаем динамические текстовые боксы для настройки датчиков на участке
+        {
+            //для датчиков слева
+            for (int index = 0; index < UCH_ZR_set_left.Length; index++)
+            {
+                UCH_ZR_set_left[index].G = new Grid();
+
+                UCH_ZR_set_left[index].UCH_ZR_Set_Address = new TextBox();
+                UCH_ZR_set_left[index].UCH_ZR_Set_Address.HorizontalAlignment= HorizontalAlignment.Center;
+                UCH_ZR_set_left[index].UCH_ZR_Set_Address.VerticalAlignment= VerticalAlignment.Center;
+                UCH_ZR_set_left[index].UCH_ZR_Set_Address.Name = "UCH_ZR_Set_Address_Index_" + index.ToString();
+                UCH_ZR_set_left[index].UCH_ZR_Set_Address.Margin= new Thickness(0,0,100,5);
+                UCH_ZR_set_left[index].UCH_ZR_Set_Address.Width = 40;
+                UCH_ZR_set_left[index].UCH_ZR_Set_Address.Height = 18;
+                UCH_ZR_set_left[index].UCH_ZR_Set_Address.Style = (Style)UCH_ZR_set_left[index].UCH_ZR_Set_Address.FindResource("TextBoxStyle_Type1");
+                UCH_ZR_set_left[index].UCH_ZR_Set_Address.PreviewTextInput += new TextCompositionEventHandler(Cheking_for_numbers);
+
+                UCH_ZR_set_left[index].UCH_ZR_Set_NP = new TextBox();
+                UCH_ZR_set_left[index].UCH_ZR_Set_NP.HorizontalAlignment = HorizontalAlignment.Center;
+                UCH_ZR_set_left[index].UCH_ZR_Set_NP.VerticalAlignment = VerticalAlignment.Center;
+                UCH_ZR_set_left[index].UCH_ZR_Set_NP.Name = "UCH_ZR_Set_NP_Index_" + index.ToString();
+                UCH_ZR_set_left[index].UCH_ZR_Set_NP.Margin = new Thickness(0, 0, 0, 5);
+                UCH_ZR_set_left[index].UCH_ZR_Set_NP.Width = 40;
+                UCH_ZR_set_left[index].UCH_ZR_Set_NP.Height = 18;
+                UCH_ZR_set_left[index].UCH_ZR_Set_NP.Style = (Style)UCH_ZR_set_left[index].UCH_ZR_Set_NP.FindResource("TextBoxStyle_Type1");
+                UCH_ZR_set_left[index].UCH_ZR_Set_NP.PreviewTextInput += new TextCompositionEventHandler(Cheking_for_numbers);
+
+                UCH_ZR_set_left[index].UCH_ZR_Set_Channel = new TextBox();
+                UCH_ZR_set_left[index].UCH_ZR_Set_Channel.HorizontalAlignment = HorizontalAlignment.Center;
+                UCH_ZR_set_left[index].UCH_ZR_Set_Channel.VerticalAlignment = VerticalAlignment.Center;
+                UCH_ZR_set_left[index].UCH_ZR_Set_Channel.Name = "UCH_ZR_Set_Channe_Index_" + index.ToString();
+                UCH_ZR_set_left[index].UCH_ZR_Set_Channel.Margin = new Thickness(100, 0, 0, 5);
+                UCH_ZR_set_left[index].UCH_ZR_Set_Channel.Width = 40;
+                UCH_ZR_set_left[index].UCH_ZR_Set_Channel.Height = 18;
+                UCH_ZR_set_left[index].UCH_ZR_Set_Channel.Style = (Style)UCH_ZR_set_left[index].UCH_ZR_Set_Channel.FindResource("TextBoxStyle_Type1");
+                UCH_ZR_set_left[index].UCH_ZR_Set_Channel.PreviewTextInput += new TextCompositionEventHandler(Cheking_for_numbers);
+
+                UCH_ZR_set_left[index].G.Children.Add(UCH_ZR_set_left[index].UCH_ZR_Set_Address);
+                UCH_ZR_set_left[index].G.Children.Add(UCH_ZR_set_left[index].UCH_ZR_Set_NP);
+                UCH_ZR_set_left[index].G.Children.Add(UCH_ZR_set_left[index].UCH_ZR_Set_Channel);
+            }
+            //для датчиков справа
+            for (int index = 0; index < UCH_ZR_set_right.Length; index++)
+            {
+                UCH_ZR_set_right[index].G = new Grid();
+
+                UCH_ZR_set_right[index].UCH_ZR_Set_Address = new TextBox();
+                UCH_ZR_set_right[index].UCH_ZR_Set_Address.HorizontalAlignment = HorizontalAlignment.Center;
+                UCH_ZR_set_right[index].UCH_ZR_Set_Address.VerticalAlignment = VerticalAlignment.Center;
+                UCH_ZR_set_right[index].UCH_ZR_Set_Address.Name = "UCH_ZR_Set_Address_Index_" + index.ToString();
+                UCH_ZR_set_right[index].UCH_ZR_Set_Address.Margin = new Thickness(0, 0, 100, 5);
+                UCH_ZR_set_right[index].UCH_ZR_Set_Address.Width = 40;
+                UCH_ZR_set_right[index].UCH_ZR_Set_Address.Height = 18;
+                UCH_ZR_set_right[index].UCH_ZR_Set_Address.Style = (Style)UCH_ZR_set_right[index].UCH_ZR_Set_Address.FindResource("TextBoxStyle_Type1");
+                UCH_ZR_set_right[index].UCH_ZR_Set_Address.PreviewTextInput += new TextCompositionEventHandler(Cheking_for_numbers);
+
+                UCH_ZR_set_right[index].UCH_ZR_Set_NP = new TextBox();
+                UCH_ZR_set_right[index].UCH_ZR_Set_NP.HorizontalAlignment = HorizontalAlignment.Center;
+                UCH_ZR_set_right[index].UCH_ZR_Set_NP.VerticalAlignment = VerticalAlignment.Center;
+                UCH_ZR_set_right[index].UCH_ZR_Set_NP.Name = "UCH_ZR_Set_NP_Index_" + index.ToString();
+                UCH_ZR_set_right[index].UCH_ZR_Set_NP.Margin = new Thickness(0, 0, 0, 5);
+                UCH_ZR_set_right[index].UCH_ZR_Set_NP.Width = 40;
+                UCH_ZR_set_right[index].UCH_ZR_Set_NP.Height = 18;
+                UCH_ZR_set_right[index].UCH_ZR_Set_NP.Style = (Style)UCH_ZR_set_right[index].UCH_ZR_Set_NP.FindResource("TextBoxStyle_Type1");
+                UCH_ZR_set_right[index].UCH_ZR_Set_NP.PreviewTextInput += new TextCompositionEventHandler(Cheking_for_numbers);
+
+                UCH_ZR_set_right[index].UCH_ZR_Set_Channel = new TextBox();
+                UCH_ZR_set_right[index].UCH_ZR_Set_Channel.HorizontalAlignment = HorizontalAlignment.Center;
+                UCH_ZR_set_right[index].UCH_ZR_Set_Channel.VerticalAlignment = VerticalAlignment.Center;
+                UCH_ZR_set_right[index].UCH_ZR_Set_Channel.Name = "UCH_ZR_Set_Channe_Index_" + index.ToString();
+                UCH_ZR_set_right[index].UCH_ZR_Set_Channel.Margin = new Thickness(100, 0, 0, 5);
+                UCH_ZR_set_right[index].UCH_ZR_Set_Channel.Width = 40;
+                UCH_ZR_set_right[index].UCH_ZR_Set_Channel.Height = 18;
+                UCH_ZR_set_right[index].UCH_ZR_Set_Channel.Style = (Style)UCH_ZR_set_right[index].UCH_ZR_Set_Channel.FindResource("TextBoxStyle_Type1");
+                UCH_ZR_set_right[index].UCH_ZR_Set_Channel.PreviewTextInput += new TextCompositionEventHandler(Cheking_for_numbers);
+
+                UCH_ZR_set_right[index].G.Children.Add(UCH_ZR_set_right[index].UCH_ZR_Set_Address);
+                UCH_ZR_set_right[index].G.Children.Add(UCH_ZR_set_right[index].UCH_ZR_Set_NP);
+                UCH_ZR_set_right[index].G.Children.Add(UCH_ZR_set_right[index].UCH_ZR_Set_Channel);
+            }
+        }
         private void Cheking_for_numbers(object sender, TextCompositionEventArgs e)
         {
-            e.Handled = !IsTextAllowed(e.Text);
+        }
 
-            //ограничиваем ввод данных в 5 символов
+
+        private static readonly Regex UCH_ZR_RangeNumbers = new Regex("[01234567]");    //набор допустимых символов для ввода в TextBox
+
+        private void Cheking_for_numbers_type1(object sender, TextCompositionEventArgs e)   //ограничение 1 символ (цифры от 0 до 7)
+        {
+            e.Handled = !UCH_ZR_RangeNumbers.IsMatch(e.Text);
+
             if (e.Handled == false)
             {
+                //ограничиваем ввод данных по количеству символов
                 int strlength = ((System.Windows.Controls.TextBox)sender).Text.Length + 1;
-                if (strlength > 5)
+                if (strlength > 1)
                 {
                     e.Handled = true;
                 }
-
             }
         }
-        private static readonly Regex onlyNumbers = new Regex("[^0-9.-]+");
-        private static bool IsTextAllowed(string text)
+
+        private void UCH_CountZR_Left_TextChanged(object sender, TextChangedEventArgs e)
         {
-            return !onlyNumbers.IsMatch(text);
+            if (UCH_Left_StPanel != null) UCH_Left_StPanel.Children.Clear();
+
+            if (UCH_CountZR_Left.Text != "")
+            {
+                for (int i = 0; i < Convert.ToInt32(UCH_CountZR_Left.Text); i++)
+                {
+                    UCH_Left_StPanel.Children.Add(UCH_ZR_set_left[i].G);
+                }
+            }
+        }
+
+        private void UCH_AddZR_Left_Click(object sender, RoutedEventArgs e) //добавить датчики на участке слева
+        {
+            int TB_Value = 0;
+
+            if (UCH_CountZR_Left.Text != "") TB_Value = Convert.ToInt32(UCH_CountZR_Left.Text);
+
+            TB_Value++;
+
+            if (UCH_ZR_RangeNumbers.IsMatch(TB_Value.ToString()))
+            {
+                UCH_CountZR_Left.Text = TB_Value.ToString();
+            }
+        }
+
+        private void UCH_RemoveZR_Left_Click(object sender, RoutedEventArgs e)  //убрать датчики на участке слева
+        {
+            int TB_Value = 0;
+
+            if (UCH_CountZR_Left.Text != "") TB_Value = Convert.ToInt32(UCH_CountZR_Left.Text);
+
+            if (TB_Value > 0)
+            {
+                TB_Value--;
+                UCH_CountZR_Left.Text = TB_Value.ToString();
+            }
+        }
+
+        private void UCH_CountZR_Right_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (UCH_Right_StPanel != null) UCH_Right_StPanel.Children.Clear();
+
+            if (UCH_CountZR_Right.Text != "")
+            {
+                for (int i = 0; i < Convert.ToInt32(UCH_CountZR_Right.Text); i++)
+                {
+                    UCH_Right_StPanel.Children.Add(UCH_ZR_set_right[i].G);
+                }
+            }
+        }
+
+        private void UCH_AddZR_Right_Click(object sender, RoutedEventArgs e)
+        {
+            int TB_Value = 0;
+
+            if (UCH_CountZR_Right.Text != "") TB_Value = Convert.ToInt32(UCH_CountZR_Right.Text);
+
+            TB_Value++;
+
+            if (UCH_ZR_RangeNumbers.IsMatch(TB_Value.ToString()))
+            {
+                UCH_CountZR_Right.Text = TB_Value.ToString();
+            }
+        }
+
+        private void UCH_RemoveZR_Right_Click(object sender, RoutedEventArgs e)
+        {
+            int TB_Value = 0;
+
+            if (UCH_CountZR_Right.Text != "") TB_Value = Convert.ToInt32(UCH_CountZR_Right.Text);
+
+            if (TB_Value > 0)
+            {
+                TB_Value--;
+                UCH_CountZR_Right.Text = TB_Value.ToString();
+            }
         }
     }
 }
